846d33a9e214efe642ed293613778a85103343af
Anurag-xo <amnrag889@gmail.com>
2025-07-09 21:03:51 +0000

    feat: basics code

---
+++ dsa-java/notes/topic-wise/basics/Primitives.java
@@ -0,0 +1,11 @@
+public class Primitives {
+
+  public static void main(String[] args) {
+    int rollno = 34;
+    char letter = 'r';
+    float marks = 32.43f;
+    double largeDecimalNumbers = 234234345343.3423;
+    long largeInteger = 123423423434L;
+    boolean check = false;
+  }
+}
