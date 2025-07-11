c9a5abfca626fd41b750504e970b93860d4d3270
Anurag-xo <amnrag889@gmail.com>
2025-07-11 18:11:27 +0000

    Refactor: Move basic Java files to src directory

--- dsa-java/notes/topic-wise/basics/Basics.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Basics {
-
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.println(input.nextLine());
-  }
-}
--- dsa-java/notes/topic-wise/basics/Method.java
+++
@@ -1,9 +0,0 @@
-public class Method {
-  static void greet() {
-    System.out.println("Hello from method!");
-  }
-
-  public static void main(String[] args) {
-    greet();
-  }
-}
