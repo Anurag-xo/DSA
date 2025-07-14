e99293a4d95b285a6b123e7d0d995983d5c38d6d
Anurag-xo <amnrag889@gmail.com>
2025-07-14 18:20:49 +0000

    Comment: add comments

--- dsa-java/notes/topic-wise/basics/src/Loops.java
+++ dsa-java/notes/topic-wise/basics/src/Loops.java
@@ -5,9 +5,11 @@ public class Loops {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
+    // This will take the input from the user
     int range = sc.nextInt();
     // Print numbers to a specified range.
 
+    // Start of the for loop (initialization)
     for (int num = 1; num <= range; num += 1) {
       System.out.println(num);
     }
