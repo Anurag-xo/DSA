aa2d3a2b70c9cf69829800bf7b3db52fe605c60f
Anurag-xo <amnrag889@gmail.com>
2026-01-07 19:47:49 +0000

    comi

--- infosys_cp/Arrays/remove_duplicates_from_sorted_array.cpp
+++ infosys_cp/Arrays/remove_duplicates_from_sorted_array.cpp
@@ -1,10 +1,10 @@
-// REmove duplicates from a sorted array (modify and return no. of unique elements)
+// Remove duplicates from a sorted array (modify and return no. of unique elements)
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int removeDuplicates(vector<int> &arr, int n) {
-  int i = 0;
+  int i = 0; // take the first element or i must say index
   for(Int j = 1; j < n ; j++){
     if (arr[i] != arr[j]){
       arr[i+1] = arr[j];
