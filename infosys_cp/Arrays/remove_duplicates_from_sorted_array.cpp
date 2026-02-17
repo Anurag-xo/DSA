4ad5bcefe0f8918d1fbf73871c24388c8edbb78f
Anurag-xo <amnrag889@gmail.com>
2026-02-17 17:14:10 +0000

    fix: typo

--- infosys_cp/Arrays/remove_duplicates_from_sorted_array.cpp
+++ infosys_cp/Arrays/remove_duplicates_from_sorted_array.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int removeDuplicates(vector<int> &arr, int n) {
   int i = 0; // take the first element or i must say index
-  for(Int j = 1; j < n ; j++){
+  for(int j = 1; j < n ; j++){
     if (arr[i] != arr[j]){
       arr[i+1] = arr[j];
       i++;
